30f473ff72aad8a2fe92ab45dcd6f841cca4c898
Andrew <was_man@mail.ru>
2018-10-01 17:23:05 +0300

    add - построчный вывод массива

---
+++ src/components/cTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    Hi
+    <table v-for = "(item, index) in arrData" :key = item>
+      <tr>
+        <td>
+          {{ item }} {{index}}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return{
+      arrData: [
+        {text1: "value 1", text2: "value 2"},
+        {text1: "value 11", text2: "value 21"},
+        {text1: "value 12", text2: "value 22"},
+        {text1: "value 13", text2: "value 23"},
+        {text1: "value 14", text2: "value 24"},
+        {text1: "value 15", text2: "value 25"},
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
