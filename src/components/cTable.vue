f27491b9f86cf54a98d11c4613ee90721e3fc514
Andrew <was_man@mail.ru>
2018-12-17 16:30:42 +0300

    макет + данные вынесены в отдельный файл

--- src/components/cTable.vue
+++ src/components/cTable.vue
@@ -1,51 +1,187 @@
 <template>
-  <div class="hello">
-    Hi
-    <table v-for = "(item, index) in arrData" :key = item>
-      <tr>
-        <td>
-          {{ item }} {{index}}
-        </td>
-      </tr>
-    </table>
-  </div>
+    <div>
+        <div id = "firstGrid">
+            <div id = "hbTable">
+                <div id = "hTable">
+                    <table id = "headTable" :width = "tHeadWidth">
+                        <tr>
+                            <td v-for="(nameRow, iKey, indNam) in tableData.info" :width = "nameRow.sizeProc">{{ nameRow.description || nameRow.name }} {{ nameRow.sizeProc }}</td>
+                        </tr>
+                        <tr v-show="filterVisible" >
+                            <!-- <td  v-for="nameRow in tableData.info"> -->
+                            <td  v-for="nameRow in tableData.info" :width = "nameRow.sizeProc">
+                            <input type = "text" width = "10" :placeholder=" nameRow.description || nameRow.name ">
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div id = "bTable">
+                    <table id = "cTable">
+                        <tr id  = "cLine" v-for="(lineContent, indContent) in tableData.content" :key="lineContent.id" @mouseover="markBack(indContent)" @mouseout="cleanMarkBack" :class = "(markTrmMouse && markRow===indContent) ? 'trBack' : '' ">
+                            <td v-for="(cell, cKey, indName) in lineContent" :width = "tableData.info[cKey].sizeProc"> {{ cell }} {{ tableData.info[cKey].sizeProc }} {{ indName }}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div id = "panFoot">
+                <button @click="showFilter()">Filter</button>
+            </div>
+        </div>
+    </div>
 </template>
-
+    
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data: function() {
-    return{
-      arrData: [
-        {text1: "value 1", text2: "value 2"},
-        {text1: "value 11", text2: "value 21"},
-        {text1: "value 12", text2: "value 22"},
-        {text1: "value 13", text2: "value 23"},
-        {text1: "value 14", text2: "value 24"},
-        {text1: "value 15", text2: "value 25"},
-      ]
+    import Vue from 'vue'
+    import {tableData1} from './tmpArray.js'
+    export default {
+        data () {
+            return{
+                tHeadWidth: 1920,
+                tBodyWidth: 1920,
+                startTableBody: 158,
+                clientWidth: window.innerWidth,
+                clientHeight: window.innerHeight,
+                markRow: '',
+                markTrmMouse: false,
+                filterVisible: false,
+                filterValue: {},
+                tableData: tableData1,
+            }
+        },
+        methods:{
+            showFilter() {
+                console.log(">start showFilter");
+                this.filterVisible = !this.filterVisible;
+                console.log("-> filter Visible: ", this.filterVisible);
+            },
+            async calcSizeHead(){
+                console.log (">start calcSizeHead");
+                console.log('==>clientHeight: ', this.clientHeight);
+                let allWidth = 0,
+                    arrWidthCell = [],
+                    keysInfo,
+                    tableInfo = this.tableData.info,
+                    indKey;
+                this.tBodyWidth = document.getElementById("bTable").clientWidth;
+                // this.tBodyWidth = document.getElementById("bTable").scrollWidth;
+                this.tHeadWidth = this.tBodyWidth;
+                alert("Body clientWidth: "+ document.getElementById("bTable").clientWidth + "\n"
+                        + "Body scrollWidth: "+ document.getElementById("bTable").scrollWidth);
+                // alert("!Head: "+ this.tHeadWidth + " Body: "+this.tBodyWidth);
+                keysInfo = Object.keys(tableInfo);
+                console.log("-> keysInfo: ", keysInfo);
+                for(indKey = 0; indKey < keysInfo.length; indKey++) {
+                    tableInfo[keysInfo[indKey]].size = Math.max(tableInfo[keysInfo[indKey]].name.length, tableInfo[keysInfo[indKey]].description.length, tableInfo[keysInfo[indKey]].size);
+                    allWidth+=tableInfo[keysInfo[indKey]].size;
+                    arrWidthCell.push({'name': keysInfo[indKey], 'size': tableInfo[keysInfo[indKey]].size})
+                    console.log("-> tableInfo.size: ", tableInfo[keysInfo[indKey]].size);
+                    console.log("-> allWidth: ", allWidth);
+                }
+                console.log("-> arrWidthCell: ", arrWidthCell);
+                this.calcWidthProc(allWidth, arrWidthCell);
+            },
+            async calcWidthProc(maxWidth, arrHead){
+                console.log (">start calcWidthProc");
+                let indArrHead;
+                console.log("=> clientWidth: ", this.clientWidth);
+                if (maxWidth > document.body.clientWidth) {
+                // if (maxWidth > this.tBodyWidthh) {
+                    console.log("=> width maxWidth: ", maxWidth);
+                    for (indArrHead = 0; indArrHead < arrHead.length; indArrHead++) {
+                        this.tableData.info[arrHead[indArrHead].name].sizeProc = String((arrHead[indArrHead].size));
+                    }
+                } else {
+                    // console.log("=> width clientWidth %: ", document.body.clientWidth);
+                    console.log("=> width clientWidth %: ", this.tBodyWidthh);
+                    for (indArrHead = 0; indArrHead < arrHead.length; indArrHead++) {
+                        this.tableData.info[arrHead[indArrHead].name].sizeProc = String((arrHead[indArrHead].size)/maxWidth*this.tBodyWidth);
+                        // this.tableData.info[arrHead[indArrHead].name].sizeProc = String((arrHead[indArrHead].size*100)/maxWidth)+'%';
+                    }
+                }
+            },
+            markBack(idRow){
+                console.log(">start markBack");
+                console.log(">start idRow: ", idRow);
+                this.markRow = idRow;
+                this.markTrmMouse = true;
+            },
+            cleanMarkBack(){
+                console.log(">start cleanMarkBack");
+                this.markTrmMouse = false;
+            },
+            customResize() {
+                this.clientWidth = window.innerWidth;
+                this.clientHeight = window.innerHeight;
+                this.calcSizeHead();
+            }
+        },
+        mounted(){
+            this.customResize();
+            console.log("===> tableData: ", this.tableData);
+            window.onresize = (event) => {
+                this.customResize();
+            };
+        }
     }
-  }
-}
 </script>
+    
+<style>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .trBack {
+        background-color: rgba(205, 215, 252, 0.925);
+    }
+    #firstGrid {
+        display: grid;
+        grid-template-rows: repeat (2, 1fr);
+        grid-template-columns: 1fr;
+        grid-gap: 0.2vw;
+        /* height: 100%; */
+        background: rgba(205, 215, 252, 0.925);
+    }
+
+    #hbTable {
+        display: grid;
+        position: relative;
+        grid-template-rows: repeat (2, 1fr);
+        grid-template-columns: 1fr;
+        grid-gap: 0.1vw;
+        height: 90vh;
+        overflow-x: auto;
+    }
+
+    #hTable{
+        width: 100%;
+        background: rgba(221, 228, 253, 0.925);
+        overflow-x: visible;
+    }
+
+    #bTable{
+        width: 100%;
+        background: rgb(255, 255, 255);
+        overflow-y: scroll;
+        overflow-x: visible;
+        padding: 0px;
+    }
+    
+    #panFoot{
+        vertical-align: middle;
+        background: rgba(221, 228, 253, 0.925);
+    }
+    #headTable{
+        text-align: center;
+        padding: 0px;
+        /* border: 1px solid grey; */
+    }
+    #cTable{
+        text-align: center;
+        border: 1px solid grey;
+        padding: 0px;
+    }
+    /* #cLine{
+        border: 1px solid grey;
+    } */
+    td{
+        border: 1px solid grey;
+    }
 </style>
+
